a59981fdf25003fc8e26c18e6fb0988c44d004ba
Mout Pessemier <moutpessemier@hotmail.com>
2024-07-29 09:47:29 +0300

    feat: add firing lasers - add sprite - add flash_component - add scale_component - add spawner_component - add laser scene - add white flash shader

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,3 +1,16 @@
 class_name Ship
 extends Node2D
 
+@onready var left_gun: Marker2D = $LeftGun
+@onready var right_gun: Marker2D = $RightGun
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var scale_component: ScaleComponent = $ScaleComponent
+
+func _ready() -> void:
+	fire_rate_timer.timeout.connect(fire_lasers)
+
+func fire_lasers() -> void:
+	spawner_component.spawn(left_gun.global_position)
+	spawner_component.spawn(right_gun.global_position)
+	scale_component.tween_scale()
