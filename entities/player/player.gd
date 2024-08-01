8b0e2553643f4db36eb215867354a0d336cd0290
Mout Pessemier <moutpessemier@hotmail.com>
2024-08-02 01:28:16 +0300

    feat: add fire & animations - turn ship animations - normal fire animation - turn fire animation

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,11 +1,14 @@
 class_name Ship
 extends Node2D
 
-@onready var left_gun: Marker2D = $LeftGun
-@onready var right_gun: Marker2D = $RightGun
-@onready var spawner_component: SpawnerComponent = $SpawnerComponent
-@onready var fire_rate_timer: Timer = $FireRateTimer
-@onready var scale_component: ScaleComponent = $ScaleComponent
+@export var left_gun: Marker2D
+@export var right_gun: Marker2D
+@export var spawner_component: SpawnerComponent
+@export var fire_rate_timer: Timer
+@export var scale_component: ScaleComponent
+@export var ship: AnimatedSprite2D
+@export var fire: AnimatedSprite2D
+@export var move_component: MoveComponent
 
 func _ready() -> void:
 	fire_rate_timer.timeout.connect(fire_lasers)
@@ -14,3 +17,17 @@ func fire_lasers() -> void:
 	spawner_component.spawn(left_gun.global_position)
 	spawner_component.spawn(right_gun.global_position)
 	scale_component.tween_scale()
+
+func _process(delta: float) -> void:
+	animate_ship()
+
+func animate_ship() -> void:
+	if move_component.velocity.x < 0:
+		ship.play("turn_left")
+		fire.play("turn_left")
+	elif move_component.velocity.x > 0:
+		ship.play("turn_right")
+		fire.play("turn_right")
+	else:
+		ship.play("center")
+		fire.play("center")
