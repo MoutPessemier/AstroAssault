1d886e52b444e7bcc31973be3d66018866f2fea2
Mout Pessemier <moutpessemier@hotmail.com>
2024-08-04 22:45:41 +0300

    feat: make lasers hit enemy + chore: update effects folder structure

--- entities/laser/laser.gd
+++ entities/laser/laser.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-@onready var visible_on_screen_notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var scale_component: ScaleComponent = $ScaleComponent
-@onready var flash_component: FlashComponent = $FlashComponent
+@export var visible_on_screen_notifier: VisibleOnScreenNotifier2D
+@export var scale_component: ScaleComponent
+@export var flash_component: FlashComponent
+@export var hitbox_component: HitboxComponent
 
 func _ready() -> void:
 	scale_component.tween_scale()
 	flash_component.flash()
 	visible_on_screen_notifier.screen_exited.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(queue_free.unbind(1))
