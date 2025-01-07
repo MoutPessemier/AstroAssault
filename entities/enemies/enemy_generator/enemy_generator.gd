2882b0e03c93991adc91b5ecc0c6d7e5c0a726a1
Mout Pessemier <moutpessemier@hotmail.com>
2025-01-07 01:42:29 +0100

    feat: add difficulty curve

--- entities/enemies/enemy_generator/enemy_generator.gd
+++ entities/enemies/enemy_generator/enemy_generator.gd
@@ -9,13 +9,20 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 @export var jolt_jumper_spawn_timer: Timer
 @export var ORBITAL_CRAWLER_SCENE: PackedScene
 @export var JOLT_JUMPER_SCENE: PackedScene
+@export var game_stats: GameStats
 
 
 func _ready() -> void:
 	orbital_crawler_spawn_timer.timeout.connect(handle_spawn.bind(ORBITAL_CRAWLER_SCENE, orbital_crawler_spawn_timer))
 	#jolt_jumper_spawn_timer.timeout.connect(handle_spawn.bind(JOLT_JUMPER_SCENE, jolt_jumper_spawn_timer))
+	
+	game_stats.score_changed.connect(func(new_score: int):
+		if new_score > 20:
+			jolt_jumper_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+	)
 
-func handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
+func handle_spawn(enemy_scene: PackedScene, timer: Timer, time_offset: float = 1.0) -> void:
 	spawner_component.scene = enemy_scene
 	spawner_component.spawn(Vector2(randf_range(MARGIN, screen_width - MARGIN), -16))
-	timer.start()
+	var spawn_rate = time_offset / (0.5 + game_stats.score * 0.01)
+	timer.start(spawn_rate + randf_range(0.25, 0.5))
--- entities/environment/environment_generator/environment_generator.gd
+++ entities/environment/environment_generator/environment_generator.gd
@@ -8,12 +8,19 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 @export var shipwreck_spawn_timer: Timer
 @export var METEORITE_SCENE: PackedScene
 @export var SHIPWRECK_SCENE: PackedScene
+@export var game_stats: GameStats
 
 func _ready() -> void:
-	meteorite_spawn_timer.timeout.connect(handle_spawn.bind(METEORITE_SCENE, meteorite_spawn_timer))
-	#shipwreck_spawn_timer.timeout.connect(handle_spawn.bind(SHIPWRECK_SCENE, shipwreck_spawn_timer))
+	meteorite_spawn_timer.timeout.connect(handle_spawn.bind(METEORITE_SCENE, meteorite_spawn_timer, 1.5))
+	#shipwreck_spawn_timer.timeout.connect(handle_spawn.bind(SHIPWRECK_SCENE, shipwreck_spawn_timer, 6.0))
+	
+	game_stats.score_changed.connect(func(new_score: int):
+		if new_score > 60:
+			shipwreck_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+	)
 
-func handle_spawn(environment_scene: PackedScene, timer: Timer) -> void:
+func handle_spawn(environment_scene: PackedScene, timer: Timer, time_offset: float = 1.0) -> void:
 	spawner_component.scene = environment_scene
 	spawner_component.spawn(Vector2(randf_range(MARGIN, screen_width - MARGIN), -16))
-	timer.start()
+	var spawn_rate = time_offset / (0.35 + game_stats.score * 0.01)
+	timer.start(spawn_rate + randf_range(0.5, 0.75))
