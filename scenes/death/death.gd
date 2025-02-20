2c02d6cec4e7bf59332bda334857b268e402d16c
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-21 00:37:26 +0100

    chore: made menus the same

--- scenes/death/death.gd
+++ scenes/death/death.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var play_again_button: TextureButton
+@export var settings_button: TextureButton
 @export var home_button: TextureButton
 @export var quit_button: TextureButton
 @export var game_stats: GameStats
@@ -9,23 +10,28 @@ extends Control
 
 func _ready() -> void:
 	score_value.text = str(game_stats.score)
-	play_again_button.pressed.connect(play_again)
-	home_button.pressed.connect(home)
-	quit_button.pressed.connect(quit)
+	play_again_button.pressed.connect(_on_play_again_buttonPressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
+	home_button.pressed.connect(_on_home_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
 	game_stats.high_score = max(game_stats.score, game_stats.high_score)
 	high_score_value.text = str(game_stats.high_score)
 
-func play_again() -> void:
+func _on_play_again_buttonPressed() -> void:
 	DataManager.save_game()
 	reset_game_state()
 	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
 
-func home() -> void:
+func _on_settings_button_pressed() -> void:
+	# TODO: add implementation to settings screen
+	pass
+
+func _on_home_button_pressed() -> void:
 	DataManager.save_game()
 	game_stats.score = 0
 	get_tree().change_scene_to_file("res://scenes/start/start.tscn")
 
-func quit() -> void:
+func _on_quit_button_pressed() -> void:
 	DataManager.save_game()
 	get_tree().quit()
 
--- scenes/pause/pause.gd
+++ scenes/pause/pause.gd
@@ -1,18 +1,30 @@
 class_name PauseMenu
 extends Control
 
-@export var settings_button: TextureButton
 @export var resume_button: TextureButton
+@export var settings_button: TextureButton
+@export var home_button: TextureButton
+@export var quit_button: TextureButton
 
 
 func _ready() -> void:
-	settings_button.pressed.connect(_on_settings_button_pressed)
 	resume_button.pressed.connect(_on_resume_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
+	home_button.pressed.connect(_on_home_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+func _on_resume_button_pressed() -> void:
+	visible = false
+	get_tree().paused = false
 
 func _on_settings_button_pressed() -> void:
 	# TODO: add implementation to settings screen
 	pass
 
-func _on_resume_button_pressed() -> void:
-	visible = false
+func _on_home_button_pressed() -> void:
 	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/start/start.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().quit()
