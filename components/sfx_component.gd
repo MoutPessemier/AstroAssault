e389e01785d2986f16734e12c7690febac0bd585
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-20 16:27:46 +0100

    chore: link sfx player to sound effects bus

--- components/sfx_component.gd
+++ components/sfx_component.gd
@@ -4,6 +4,9 @@ extends AudioStreamPlayer
 @export var min_pitch: float = 0.6
 @export var max_pitch: float = 1.2
 
+func _init() -> void:
+	set_bus("SoundEffects")
+
 func play_sfx():
 	pitch_scale = randf_range(min_pitch, max_pitch)
 	play()
