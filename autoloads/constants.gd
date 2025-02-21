e733b0a21c1658e8f4d34ab1b9bcd249a69cdb96
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-21 22:30:13 +0100

    feat: introduce constant values

---
+++ autoloads/constants.gd
@@ -0,0 +1,9 @@
+extends Node
+
+const MASTER_KEY = "Master"
+const MUSIC_KEY = "Music"
+const SFX_KEY = "SoundEffects"
+
+var master_bus_index = AudioServer.get_bus_index(MASTER_KEY)
+var music_bus_index = AudioServer.get_bus_index(MUSIC_KEY)
+var sfx_bus_index = AudioServer.get_bus_index(SFX_KEY)
--- scenes/settings/Settings.gd
+++ scenes/settings/Settings.gd
@@ -2,10 +2,6 @@ extends Control
 
 @export var back_button: TextureButton
 
-var master_bus_index = AudioServer.get_bus_index("Master")
-var music_bus_index = AudioServer.get_bus_index("Music")
-var sfx_bus_index = AudioServer.get_bus_index("SoundEffects")
-
 func _ready() -> void:
 	back_button.pressed.connect(_on_back_button_pressed)
 
