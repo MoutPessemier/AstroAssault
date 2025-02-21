3ea9ed541d0b34cef33f99d85c582cc9b440904b
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-21 19:07:24 +0100

    chore: move reset to GameManager

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,3 +1,8 @@
 extends Node
 
 var game_stats: GameStats = load("res://resources/game_stats/game_stats.tres")
+
+func reset_game_state():
+	game_stats.score = 0
+	game_stats.time = 0
+	game_stats.health = 3
