2e5198f57d3fc9ab58c81b6d89e87e3557e1c269
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-23 15:39:51 +0100

    feat: save and load music settings correctly

--- autoloads/data_manager.gd
+++ autoloads/data_manager.gd
@@ -1,40 +1,47 @@
 extends Node
 
-const PATH := "user://data.save"
+const PATH_GAME_DATA := "user://data.save"
+const PATH_SETTINGS_DATA := "user://settings.save"
 
 var game_stats: GameStats = preload("res://resources/game_stats/game_stats.tres")
 var music_stats: MusicStats = preload("res://resources/music_stats/music_stats.tres")
 
 func save_game() -> void:
-	var file = FileAccess.open(PATH, FileAccess.WRITE)
+	var file = FileAccess.open(PATH_GAME_DATA, FileAccess.WRITE)
 	file.store_var(game_stats.high_score)
 
 func load_game() -> void:
-	if FileAccess.file_exists(PATH):
-		var file = FileAccess.open(PATH, FileAccess.READ)
+	if FileAccess.file_exists(PATH_GAME_DATA):
+		var file = FileAccess.open(PATH_GAME_DATA, FileAccess.READ)
 		game_stats.high_score = file.get_var(game_stats.high_score)
 	else:
 		print("No data saved to file")
 		game_stats.high_score = 0
 
 func save_settings() -> void:
-	var file = FileAccess.open(PATH, FileAccess.WRITE)
-	file.store_var(music_stats.master_value)
-	file.store_var(music_stats.music_value)
-	file.store_var(music_stats.sfx_value)
+	var settings_data = {
+		Constants.MASTER_KEY: music_stats.master_value,
+		Constants.MUSIC_KEY: music_stats.music_value,
+		Constants.SFX_KEY: music_stats.sfx_value
+	}
+	var file = FileAccess.open(PATH_SETTINGS_DATA, FileAccess.WRITE)
+	file.store_var(settings_data)
 
 func load_settings() -> void:
-	if FileAccess.file_exists(PATH):
-		var file = FileAccess.open(PATH, FileAccess.READ)
-		music_stats.master_value = file.get_var(music_stats.master_value)
-		music_stats.music_value = file.get_var(music_stats.music_value)
-		music_stats.sfx_value = file.get_var(music_stats.sfx_value)
-		print("values are:")
-		print(music_stats.master_value)
-		print(music_stats.music_value)
-		print(music_stats.sfx_value)
+	if FileAccess.file_exists(PATH_SETTINGS_DATA):
+		var file = FileAccess.open(PATH_SETTINGS_DATA, FileAccess.READ)
+		var data = file.get_var()
+		music_stats.master_value = data.get(Constants.MASTER_KEY)
+		music_stats.music_value = data.get(Constants.MUSIC_KEY)
+		music_stats.sfx_value = data.get(Constants.SFX_KEY)
 	else:
 		print("No data saved to file")
 		music_stats.master_value = 1
 		music_stats.music_value = 0.85
 		music_stats.sfx_value = 1
+	_apply_audio_settings()
+
+func _apply_audio_settings() -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(Constants.MASTER_KEY), linear_to_db(music_stats.master_value))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(Constants.MUSIC_KEY), linear_to_db(music_stats.music_value))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(Constants.SFX_KEY), linear_to_db(music_stats.sfx_value))
--- ui_components/sliders/Slider.gd
+++ ui_components/sliders/Slider.gd
@@ -6,14 +6,16 @@ var bus_index: int
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
-	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	value = DataManager.music_stats.get_matching_bus(bus_name)
+	tooltip_text = str(value * 100) + "%"
 	value_changed.connect(_on_value_changed)
 	mouse_exited.connect(_on_mouse_exit_slider)
 
 func _on_value_changed(value: float) -> void:
-	var linear_value = linear_to_db(value)
-	AudioServer.set_bus_volume_db(bus_index, linear_value)
-	DataManager.music_stats.update_bus_value(bus_name, linear_value)
+	var db_value = linear_to_db(value)
+	tooltip_text = str(value * 100) + "%"
+	AudioServer.set_bus_volume_db(bus_index, db_value)
+	DataManager.music_stats.update_bus_value(bus_name, value)
 	
 
 func _on_mouse_exit_slider() -> void:
