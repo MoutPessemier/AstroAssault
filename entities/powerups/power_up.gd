00a390fc8a55fdd9dff2f47650e0b9b2eb1deda5
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-02-28 15:49:06 +0100

    chore: fix playing pick-up sfx

--- entities/powerups/power_up.gd
+++ entities/powerups/power_up.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var pickup_range: Area2D
 @export var move_component: MoveComponent
 @export var sfx_component: SfxComponent
+@export var animated_sprite_2d: AnimatedSprite2D
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
@@ -14,6 +15,7 @@ func _ready() -> void:
 func _on_power_up_pick_up(hurtbox: HurtboxComponent) -> void:
 	if hurtbox.get_parent().name == "Player":
 		var ship: Ship = hurtbox.get_parent()
+		animated_sprite_2d.visible = false
 		sfx_component.play_sfx()
 		_apply_power_up(ship)
 		await get_tree().create_timer(0.3).timeout
