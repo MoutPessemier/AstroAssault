73ac322b09a9745fe24fd8c272dfedc621d52811
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-04-25 13:57:25 +0200

    chore: do movement in _physics_process instead of _process

--- components/border_bounce_component.gd
+++ components/border_bounce_component.gd
@@ -8,7 +8,7 @@ extends Node
 var left_border = 0
 var right_border = ProjectSettings.get_setting("display/window/size/viewport_width")
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if entity.global_position.x < left_border + margin:
 		entity.global_position.x = left_border + margin
 		move_component.velocity = move_component.velocity.bounce(Vector2.RIGHT)
